6f61a877b179a05ae7a262a0fc86c0e33c61008b
OrangeJus <balurana.baau@gmail.com>
2023-09-07 13:30:43 +0700

    Finish Part 2: Models and the admin site

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.db import models 
+from django.db import models
 from django.utils import timezone
 
 
@@ -9,19 +9,17 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200, default="Pertanyaan Default")
     pub_date = models.DateTimeField("date published")
 
-
     def __str__(self):
         return self.question_text
-    
-    
+
+    # def __str__(self) -> str:
+    #     return self.question_text
+
+    # def __str__(self) -> str:
+    #     return self.question_text
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-    # def __str__(self) -> str:
-    #     return self.question_text
-
-    # def __str__(self) -> str:
-    #     return self.question_text
 
 
 class Choice(models.Model):
